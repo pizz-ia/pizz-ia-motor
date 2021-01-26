3a81da02e17e1c28dd44c196d28d684d6bce3d63
Pablo Colson <colson.pablo@icloud.com>
2021-01-26 11:21:57 +0100

    Auto stash before merge of "master" and "origin/master"

--- src/app.py
+++ src/app.py
@@ -5,6 +5,26 @@ from camera.Camera import Camera
 from lcd.LCD import LCD
 import os
 
+def display_on_lcd(message):
+    lcd_rs = 25
+    lcd_en = 24
+    lcd_d4 = 23
+    lcd_d5 = 17
+    lcd_d6 = 18
+    lcd_d7 = 22
+    lcd_backlight = 2
+
+    # Define LCD column and row size for 16x2 LCD.
+    lcd_columns = 16
+    lcd_rows = 2
+
+    lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+    
+    lcd.clear()
+    lcd.message('OLIVES\nPEPERONI')
+    time.sleep(5.0)
+    
+
 SAVE_PICTURE_PATH = "/data"
 S3_ENDPOINT_URL = "https://object-store.par1.openstack.unyc.io"
 S3_BUCKET_NAME = "capture"
